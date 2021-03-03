22deb246b49e070b4e444e29c07dfb469fe1561d
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-03-02 21:14:08 -0300

    Added .env file to dont show pass and user emails destiny

--- src/config/smtp.js
+++ src/config/smtp.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
+console.log(process.env.USER, process.env.PASS);
+
 module.exports = {
     host: "smtp.gmail.com",
     port: "587",
-    user: "contatoandersonarorj@gmail.com",
-    password: "contato10203040"
+    user: process.env.NAME_USER,
+    password: process.env.PASS
 }
